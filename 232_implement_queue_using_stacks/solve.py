a910968499f9d5d77c55919b7b09bb047a793ac4
Benedict <z5457257@ad.unsw.edu.au>
2024-10-22 01:36:14 +1100

    STOPPED at 278

--- 232_implement_queue_using_stacks/solve.py
+++ 232_implement_queue_using_stacks/solve.py
@@ -1,19 +1,33 @@
 class MyQueue:
 
     def __init__(self):
-        
+        self.s1 = []
+        self.s2 = []
 
     def push(self, x: int) -> None:
-        
+        self.s1.append(x)
 
     def pop(self) -> int:
-        
+        if len(self.s2) != 0:
+            return self.s2.pop()
+        else:
+            while len(self.s1) != 0:
+                self.s2.append(self.s1.pop())
+            return self.s2.pop()
 
     def peek(self) -> int:
-        
+        if len(self.s2) != 0:
+            return self.s2[-1]
+        else:
+            while len(self.s1) != 0:
+                self.s2.append(self.s1.pop())
+            return self.s2[-1]
 
     def empty(self) -> bool:
-        
+        if len(self.s1) == 0 and len(self.s2) == 0:
+            return True
+        else:
+            return False
 
 
 # Your MyQueue object will be instantiated and called as such:
@@ -22,3 +36,13 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+
+def main():
+    obj = MyQueue()
+    obj.push(x)
+    param_2 = obj.pop()
+    param_3 = obj.peek()
+    param_4 = obj.empty()
+
+if __name__ == "__main__":
+    main()
---
+++ 278_first_bad_version/solve.py
@@ -0,0 +1,11 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        
+
+def main():
+
+if __name__ == "__main__":
+    main()
