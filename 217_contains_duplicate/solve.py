46ca376719ffc47917f235e828afcb9469200276
Benedict <z5457257@ad.unsw.edu.au>
2024-10-23 19:32:10 +1100

    added some problems

---
+++ 217_contains_duplicate/solve.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        return False
+
+def main():
+    x = Solution()
+    print(x.containsDuplicate([]))
+
+if __name__ == "__main__":
+    main()
