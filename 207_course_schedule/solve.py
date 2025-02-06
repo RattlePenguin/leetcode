27b52fa7f87693769a12c99423f2bb00c6ed5a4d
RattlePenguin <viggo.setiawan@gmail.com>
2025-02-06 10:46:14 +0800

    207 incomplete, dfs and top sort to find cycles

---
+++ 207_course_schedule/solve.py
@@ -0,0 +1,43 @@
+from typing import List
+class Node():
+    def __init__(self, val = 0, neighbours = None):
+        self.val = val
+        self.neighbours = neighbours if neighbours is not None else []
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        nodes = []
+        for i in range(numCourses):
+            nodes.append(None)
+        print(nodes)
+        
+        for pr in prerequisites:
+            course = pr[0]
+            p = pr[1]
+            if nodes[course] is None:
+                courseNode = Node(course)
+                nodes[course] = courseNode
+            else:
+                courseNode = nodes[course]
+            if nodes[p] is None:
+                pNode = Node(p)
+                nodes[p] = pNode
+            else:
+                pNode = nodes[p]
+            
+            pNode.neighbours.append(courseNode)
+        
+        
+
+def main():
+    x = Solution()
+    print(x.canFinish(2, [[1,0]]))
+    print(x.canFinish(2, []))
+    print(x.canFinish(2, [[1,0],[0,1]]))
+    print(x.canFinish(3, [[1,0],[0,1]]))
+    print(x.canFinish(3, [[1,0],[2,0],[2,1]]))
+    print(x.canFinish(3, [[0,1],[1,2],[2,0]]))
+
+
+if __name__ == "__main__":
+    main()
